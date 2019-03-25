97263a916febc5fcd1647d162464d1315737b8d1
Tanguy Esteoule <tanguy.esteoule@meteoswift.com>
2019-03-25 17:52:24 +0100

    new version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='links_pydriver',
-      version='0.1',
+      version='0.3',
       description='A python driver for Links',
       url='https://github.com/tanguyesteoule/links_pydriver',
       author='Tanguy Esteoule',
       author_email='tanguy.esteoule@gmail.com',
       license='MIT',
-      packages=['links_pydriver'],
+      packages = find_packages(),
       zip_safe=False)
